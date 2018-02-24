a22e96583b8328cb33bdc937bf41281391e6756e
contra <contra@users.noreply.github.com>
2018-02-23 19:02:44 -0500

    add tile38 plist
    basically a copy of the redis plist

--- Formula/tile38.rb
+++ Formula/tile38.rb
@@ -40,6 +40,36 @@ class Tile38 < Formula
     EOS
   end
 
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/tile38-server</string>
+          <string>-d #{datadir}</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/tile38.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/tile38.log</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
   test do
     system bin/"tile38-cli", "-h"
   end
